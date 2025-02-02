f1d34d56cf9ceaf6e34ad1b2d57631311b762c44
Musthafa AM <musthafaanjilakadavathu399@gmail.com>
2025-02-02 10:07:52 +0530

    Fix css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 "use client";
 import { AuthProvider } from "@/context/AuthContext"; 
 import Navbar from "@/components/Navbar"; 
-import "@/styles/globals.css"; 
+import "../styles/globals.css";; 
 
 export default function RootLayout({ children }) {
   return (
