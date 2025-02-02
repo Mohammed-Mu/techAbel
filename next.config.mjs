5f89219b273e2d2b9a7c074f568cc22e3738a517
Musthafa AM <musthafaanjilakadavathu399@gmail.com>
2025-02-02 10:55:02 +0530

    Updates

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-    output: 'export',  // This enables static export for Next.js 13
-    basePath: isProd ? '/techAbel' : '',  // Replace with your repo name
-    assetPrefix: isProd ? '/techAbel/' : '',  // Adjust asset paths
-  };
-
-export default nextConfig;
